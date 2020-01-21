c9c7e23082fb5938503b2bfb4fc4bddddbb47fce
MirInjamamul <injamul120221@gmail.com>
2020-01-21 13:32:26 +0600

    added express.js

--- example.js
+++ example.js
@@ -1,2 +1,26 @@
 // Identify Object Model
+    /*
+        USER
+            -name
+            -id
+            -email
+            -password
+            -birthday
+            -phoneno
+        
+            POST
+                -title
+                -body
+                -[comments]
+                -likes
+                -createdTime
+                -updatedTime
+                
+    */
+// Create Object URI
 
+// Determine Represantation
+
+// Single Data Representation
+
+//JSON View
