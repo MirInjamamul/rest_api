1aff9f6d6fa9e63e46d2c31138e130d3a1c0b5ff
injamamul <injamul120221@gmail.com>
2020-01-25 11:12:31 +0600

    init proj

--- example.js
+++ example.js
@@ -1,26 +1,8 @@
 // Identify Object Model
-    /*
-        USER
-            -name
-            -id
-            -email
-            -password
-            -birthday
-            -phoneno
-        
-            POST
-                -title
-                -body
-                -[comments]
-                -likes
-                -createdTime
-                -updatedTime
-                
-    */
-// Create Object URI
+
+// Create Model URI
 
 // Determine Represantation
-
 // Single Data Representation
 
 //JSON View
